2f113700b1cca83bd2e875ffc7de277ab3c667af
JRBoland <james.boland94@hotmail.com>
2023-02-02 13:34:37 +1100

    Added config to main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def create_app():
+    #using a list comprehension and multiple assignment
+    #to grab the environment variables we need
+
+    #creating the flask app object - the core of our app!
+    app = Flask(__name__)
+
+    #configuring our app:
+    app.config.from_object("config.app_config")
+
+    #creating our database object, allowing us to use our ORM
+    db = SQLAlchemy(app)
+
+    return app
+
+
